e7bf2960f996bf151f0201c3b8a0a089d2992fc9
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2021-01-04 12:05:55 +0100

    improved hover effect

--- Sources/gameLoop/dependentModules/inventoryRenderer/inventoryRenderer.cpp
+++ Sources/gameLoop/dependentModules/inventoryRenderer/inventoryRenderer.cpp
@@ -11,7 +11,7 @@
 #include "itemEngine.hpp"
 #include "singleWindowLibrary.hpp"
 
-ogl::rect inventory_slots[20], select_rect;
+ogl::rect inventory_slots[20], select_rect, under_text_rect(ogl::top_left);
 
 #define MARGIN 10
 
@@ -31,26 +31,34 @@ void inventoryRenderer::init() {
     select_rect.setWidth(2 * BLOCK_WIDTH + 2 * MARGIN);
     select_rect.setHeight(2 * BLOCK_WIDTH + 2 * MARGIN);
     select_rect.setY(MARGIN / 2);
+    
+    under_text_rect.setColor(0, 0, 0);
 }
 
+#define MARGIN 10
+
 void inventoryRenderer::render() {
-    select_rect.setX(
-            short((itemEngine::selected_slot - 5) * (2 * BLOCK_WIDTH + 2 * MARGIN) + 2 * BLOCK_WIDTH / 2 +
-                               MARGIN));
+    select_rect.setX(short((itemEngine::selected_slot - 5) * (2 * BLOCK_WIDTH + 2 * MARGIN) + 2 * BLOCK_WIDTH / 2 + MARGIN));
     select_rect.render();
     ogl::texture* text_texture = nullptr;
     for(int i = 0; i < (itemEngine::inventory_open ? 20 : 10); i++) {
-        if(swl::colliding(inventory_slots[i].getRect(), {swl::mouse_x, swl::mouse_y, 0, 0}) && itemEngine::inventory_open) {
+        if(swl::colliding(inventory_slots[i].getRect(), {swl::mouse_x, swl::mouse_y, 0, 0}) && itemEngine::inventory_open && itemEngine::inventory[i].item_id != itemEngine::NOTHING) {
             inventory_slots[i].setColor(70, 70, 70);
             text_texture = &itemEngine::inventory[i].getUniqueItem().text_texture;
-            text_texture->setX(swl::mouse_x);
-            text_texture->setY(swl::mouse_y);
+            text_texture->setX(swl::mouse_x + 20);
+            text_texture->setY(swl::mouse_y + 20);
+            under_text_rect.setHeight(text_texture->getHeight() + 2 * MARGIN);
+            under_text_rect.setWidth(text_texture->getWidth() + 2 * MARGIN);
+            under_text_rect.setX(swl::mouse_x + 20 - MARGIN);
+            under_text_rect.setY(swl::mouse_y + 20 - MARGIN);
         }
         else
             inventory_slots[i].setColor(100, 100, 100);
         inventory_slots[i].render();
         itemEngine::inventory[i].render(inventory_slots[i].getX() + MARGIN / 2, inventory_slots[i].getY() + MARGIN / 2);
     }
-    if(text_texture)
+    if(text_texture) {
+        under_text_rect.render();
         text_texture->render();
+    }
 }
