15b9f67ce1cce1dbe0d62f92e341d41e1a0156be
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2021-02-26 13:46:07 +0100

    fixed a bug

--- Sources/core/packets/packets.cpp
+++ Sources/core/packets/packets.cpp
@@ -56,7 +56,7 @@ packets::packet packets::getPacket(int socket) {
     }
     
     // if bytes_received is 0 that means that the other side disconnected
-    if(bytes_received) {
+    if(bytes_received > 0) {
         // size of the packet are the first two bytes
         unsigned short size = buffer[0] + (buffer[1] << 8);
         // packet type is the third byte
