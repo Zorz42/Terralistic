8fbf12813472b9cf40d1a3f3be6c0fb7cf882f90
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2021-03-20 17:33:19 +0100

    a

--- Client/Sources/main/main.cpp
+++ Client/Sources/main/main.cpp
@@ -27,3 +27,37 @@ int main(int argc, char **argv) {
     
     return 0;
 }
+
+/*#include <Graphics/graphics.hpp>
+
+struct scene : public gfx::scene {
+    gfx::sprite hello_text;
+    void init() {
+        hello_text.setTexture(gfx::renderText("hello", {255, 255, 255}));
+        hello_text.orientation = gfx::center;
+        hello_text.scale = 3;
+    }
+    
+    void render() {
+        gfx::render(hello_text);
+    }
+    
+    void stop() {
+        
+    }
+};
+
+int main() {
+    
+    gfx::init(1000, 600);
+    gfx::loadFont("pixel_font.ttf", 8);
+    gfx::resource_path = "../Resources";
+    gfx::setWindowMinimumSize(gfx::getWindowWidth(), gfx::getWindowHeight());
+    
+    gfx::switchScene(new scene());
+    gfx::runScenes();
+    
+    gfx::quit();
+    
+    return 0;
+}*/
