f749fbceec3ab27b42502896d344eb1ad41eadb4
iKramp <60236692+iKramp@users.noreply.github.com>
2021-06-01 19:08:00 +0200

    added a comicaly large switch lol

--- Client/Sources/game/Modules/clientMap/block.cpp
+++ Client/Sources/game/Modules/clientMap/block.cpp
@@ -115,11 +115,81 @@ void map::block::updateOrientation() {
 
 void map::block::draw() {
     SimplexNoise noise(0);
-    gfx::rect rect((x & 15) * BLOCK_WIDTH, (y & 15) * BLOCK_WIDTH, BLOCK_WIDTH, BLOCK_WIDTH, {0, 0, 0, (unsigned char)(255 - 255.0 / MAX_LIGHT * getLightLevel())});
-    float biome = noise.noise((float)x / 1100 + 0.125);
-    gfx::rect rectBiomeTest((x & 15) * BLOCK_WIDTH, (y & 15) * BLOCK_WIDTH, BLOCK_WIDTH, BLOCK_WIDTH, {(unsigned char)biome, (unsigned char)biome, 100, (unsigned char)255 });
+    unsigned short heat = (int)((noise.noise((float)x / 550 + 0.25) + 1) * 3);
+    heat = heat / 2;
+    if(heat == 3)
+        heat = 2;
+
+    unsigned short biomeheight = (int)((noise.noise((float)x / 300 + 0.001) + 1) * 4);
+    biomeheight /= 2;
+    if(biomeheight == 4)
+        biomeheight = 3;
+
+    gfx::rect rect((x & 15) * BLOCK_WIDTH, (y & 15) * BLOCK_WIDTH, BLOCK_WIDTH, BLOCK_WIDTH, { 0, 0, 0, (unsigned char)(255 - 255.0 / MAX_LIGHT * getLightLevel()) });
+    gfx::rect rectBiomeHeight((x & 15) * BLOCK_WIDTH, (y & 15) * BLOCK_WIDTH, BLOCK_WIDTH, BLOCK_WIDTH, { 200, (unsigned char)biomeheight, (unsigned char)biomeheight, (unsigned char)255 });
+    gfx::rect rectHeat((x & 15) * BLOCK_WIDTH, (y & 15) * BLOCK_WIDTH, BLOCK_WIDTH, BLOCK_WIDTH, { (unsigned char)heat, (unsigned char)heat, 200, (unsigned char)255 });
+    gfx::rect biome;
 
 
+    switch (heat)
+    {
+    case 0:
+        switch (biomeheight)
+        {
+        case 0://icy seas
+            
+            break;
+        case 1://snowy tundra
+
+            break;
+        case 2://cold hills (with taiga trees?)
+
+            break;
+
+        case 3://snowy mountains
+
+            break;
+        }
+        break;
+
+    case 1:
+        switch (biomeheight)
+        {
+        case 0://regular sea
+
+            break;
+        case 1://plains
+
+            break;
+        case 2://forest
+
+            break;
+
+        case 3://regular mountain
+
+            break;
+        }
+        break;
+
+    case 2:
+        switch (biomeheight)
+        {
+        case 0://warm ocean
+
+            break;
+        case 1://desert
+
+            break;
+        case 2://savana
+
+            break;
+
+        case 3://savana mountains
+
+            break;
+        }
+        break;
+    }
 
     if(block_data->getUniqueBlock().texture.getTexture() && getLightLevel())
         gfx::render(block_data->getUniqueBlock().texture, rect.x, rect.y, gfx::rectShape(0, short((BLOCK_WIDTH >> 1) * block_data->orientation), BLOCK_WIDTH >> 1, BLOCK_WIDTH >> 1));
@@ -131,7 +201,10 @@ void map::block::draw() {
         gfx::render(breaking_texture, rect.x, rect.y, gfx::rectShape(0, short(BLOCK_WIDTH / 2 * (getBreakStage() - 1)), BLOCK_WIDTH / 2, BLOCK_WIDTH / 2));
 
     if (getLightLevel() <= MAX_LIGHT / 4)
-      gfx::render(rectBiomeTest);
+        if ((x & 1) ^ (y & 1) == 1)
+            gfx::render(rectBiomeHeight);
+        else
+            gfx::render(rectHeat);
 
 }
 
