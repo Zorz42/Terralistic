7635d6e8c66d4dc89624259e5adc4ff9efe61aa6
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2021-06-18 14:49:24 +0200

    shortened build script

--- build.py
+++ build.py
@@ -2,22 +2,27 @@ import os
 import sys
 import shutil
 
+project_path = os.path.dirname(os.path.realpath(__file__)) + "/"
+
+
+def createDir(path):
+    if not os.path.exists(project_path + path):
+        os.mkdir(project_path + path)
+
+
 if sys.platform == "darwin":
-    project_path = os.path.dirname(os.path.realpath(__file__)) + "/"
-    if not os.path.exists(project_path + "Dependencies/"):
-        os.mkdir(project_path + "Dependencies/")
+    createDir("Dependencies/")
     if not os.path.exists(project_path + "Dependencies/MacOS/"):
-        os.system(f"git clone https://github.com/Zorz42/Terralistic-MacOS-Dependecies {project_path + 'Dependencies/MacOS/'}")
+        os.system(
+            f"git clone https://github.com/Zorz42/Terralistic-MacOS-Dependecies {project_path + 'Dependencies/MacOS/'}")
     else:
         os.system(f"git -C {project_path + 'Dependencies/MacOS/'} pull --rebase")
 
-    os.system(f"xcodebuild build -project {project_path + 'Terralistic.xcodeproj'} ARCHS=x86_64 ONLY_ACTIVE_ARCH=NO -scheme Terralistic BUILD_DIR={project_path + 'Temp'}")
+    os.system(
+        f"xcodebuild build -project {project_path + 'Terralistic.xcodeproj'} ARCHS=x86_64 ONLY_ACTIVE_ARCH=NO -scheme Terralistic BUILD_DIR={project_path + 'Temp'}")
 
-    if not os.path.exists(project_path + "Output/"):
-        os.mkdir(project_path + "Output")
-
-    if not os.path.exists(project_path + "Output/MacOS/"):
-        os.mkdir(project_path + "Output/MacOS/")
+    createDir("Output/")
+    createDir("Output/MacOS/")
 
     if os.path.exists(project_path + "Output/MacOS/Terralistic.app/"):
         shutil.rmtree(project_path + "Output/MacOS/Terralistic.app/")
@@ -27,20 +32,13 @@ if sys.platform == "darwin":
 elif sys.platform == "linux":
     os.system("sudo apt install libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev")
 
-    project_path = os.path.dirname(os.path.realpath(__file__)) + "/"
-    if not os.path.exists(project_path + "Build/"):
-        os.mkdir(project_path + "Build/")
+    createDir("Build/")
 
     os.system(f"cd {project_path + 'Build/'} && cmake .. && make -j$(nproc)")
 
-    if not os.path.exists(project_path + "Output/"):
-        os.mkdir(project_path + "Output")
-
-    if not os.path.exists(project_path + "Output/Linux/"):
-        os.mkdir(project_path + "Output/Linux/")
-
-    if not os.path.exists(project_path + "Output/Linux/Terralistic/"):
-        os.mkdir(project_path + "Output/Linux/Terralistic/")
+    createDir("Output/")
+    createDir("Output/Linux/")
+    createDir("Output/Linux/Terralistic")
 
     if os.path.exists(project_path + "Output/Linux/Terralistic/Resources/"):
         shutil.rmtree(project_path + "Output/Linux/Terralistic/Resources/")
