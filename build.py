a7b9c0664c8bf83465133a227d3f893c6d74deca
Zorz42 <jakob@zorz.si>
2021-06-19 19:20:11 +0200

    fixed a stupid mistake

--- build.py
+++ build.py
@@ -8,7 +8,6 @@ project_path = os.path.dirname(os.path.realpath(__file__)) + "/"
 def createDir(path):
     os.makedirs(project_path + path, exist_ok=True)
 
-
 if sys.platform == "darwin":
     createDir(project_path + "Dependencies/")
     if not os.path.exists(project_path + "Dependencies/MacOS/"):
@@ -18,7 +17,7 @@ if sys.platform == "darwin":
 
     os.system(f"xcodebuild build -project {project_path}Terralistic.xcodeproj ARCHS=x86_64 ONLY_ACTIVE_ARCH=NO -scheme Terralistic BUILD_DIR={project_path}Temp")
 
-    createDir(project_path + "Output/MacOS/")
+    createDir("Output/MacOS/")
 
     shutil.rmtree(project_path + "Output/MacOS/Terralistic.app/", ignore_errors=True)
     shutil.move(project_path + "Temp/Debug/Terralistic.app/", project_path + "Output/MacOS/")
@@ -26,10 +25,10 @@ if sys.platform == "darwin":
 elif sys.platform == "linux":
     os.system("sudo apt install libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev")
 
-    createDir(project_path + "Build/")
-    os.system(f"cd {project_path}Build/ && cmake .. && make -j$(nproc)")
+    createDir("Build/")
+    os.system(f"cd {project_path}Build/ && cmake -DCMAKE_CXX_COMPILER=/usr/bin/clang++ .. && make -j$(nproc)")
 
-    createDir(project_path + "Output/Linux/Terralistic")
+    createDir("Output/Linux/Terralistic")
 
     if os.path.exists(project_path + "Output/Linux/Terralistic/Terralistic"):
         os.remove(project_path + "Output/Linux/Terralistic/Terralistic")
@@ -38,7 +37,7 @@ elif sys.platform == "linux":
     shutil.move(project_path + "Build/Resources/", project_path + "Output/Linux/Terralistic/")
 
     shutil.copy("/usr/lib/x86_64-linux-gnu/libSDL2-2.0.so.0", project_path + "Output/Linux/Terralistic/")
-    shutil.copy("/usr/lib/x86_64-linux-gnu/llibSDL2_image-2.0.so.0", project_path + "Output/Linux/Terralistic/")
+    shutil.copy("/usr/lib/x86_64-linux-gnu/libSDL2_image-2.0.so.0", project_path + "Output/Linux/Terralistic/")
     shutil.copy("/usr/lib/x86_64-linux-gnu/libSDL2_image-2.0.so.0", project_path + "Output/Linux/Terralistic/")
 elif sys.platform == "win32":
     createDir(project_path + "Dependencies/")
@@ -47,7 +46,7 @@ elif sys.platform == "win32":
     else:
         os.system(f"git -C {project_path}Dependencies/Windows/ pull --rebase")
 
-    createDir(project_path + "Build/")
+    createDir("Build/")
     clion_folder = ""
     for folder in os.listdir("C:\\Program Files\\JetBrains\\"):
         if folder.startswith("CLion"):
@@ -57,7 +56,7 @@ elif sys.platform == "win32":
 
     os.system(f"cd {project_path}Build/ && {cmake_path} .. && {cmake_path} --build .")
 
-    createDir(project_path + "Output/Windows/Terralistic")
+    createDir("Output/Windows/Terralistic")
 
     if os.path.exists(project_path + "Output/Windows/Terralistic/"):
         os.remove(project_path + "Output/Windows/Terralistic/")
