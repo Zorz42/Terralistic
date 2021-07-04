e92212ff864a277d4e8816e13572c16d46856b53
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2021-07-04 12:35:16 +0200

    added jaclang that echoes file path back

---
+++ Jaclang/Compiler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Jaclang
+//
+//  Created by Jakob Zorz on 04/07/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::string file = argv[1];
+    std::cout << file << std::endl;
+    
+    return 0;
+}
