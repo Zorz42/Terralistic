23caa3a6bf71c41c1e90d74ce74b2013757d8f60
Oki1 <9822592+Oki1@users.noreply.github.com>
2021-07-09 21:05:49 +0200

    refactored configManager

--- Libraries/configManager/configManager.cpp
+++ Libraries/configManager/configManager.cpp
@@ -10,30 +10,47 @@
 #include <utility>
 
 configFile::configFile(const std::string& path) : path(path) {
+    loadConfig();
+}
+
+void configFile::loadConfig() {
     std::ifstream file(path);
     std::string line;
-    while(std::getline(file, line)) {
-        if(line.empty())
+    while (std::getline(file, line)) {
+        if (line.empty())
             continue;
         int separator_index = 0;
-        while(line[separator_index] != ':')
+        while (line[separator_index] != ':')
             separator_index++;
         values[line.substr(0, separator_index)] = line.substr(separator_index + 1, line.size());
     }
 }
 
-std::string configFile::get(const std::string& key) {
+std::string configFile::getStr(const std::string& key) {
     return values[key];
 }
 
-void configFile::set(const std::string& key, std::string value) {
-    values[key] = std::move(value);
+int configFile::getInt(const std::string& key) {
+    return std::stoi(values[key]);
 }
 
-void configFile::setDefault(const std::string& key, std::string value) {
-    if(values.find(key) == values.end())
-        set(key, std::move(value));
+void configFile::setStr(const std::string& key, std::string value) {
+    values[key] = std::move(value);
 }
+void configFile::setInt(const std::string& key, int value) {
+    values[key] = std::move(std::to_string(value));
+}
+
+void configFile::setDefaultStr(const std::string& key, std::string value) {
+    if(values.find(key) == values.end())
+        setStr(key, std::move(value));
+}
+
+void configFile::setDefaultInt(const std::string& key, int value) {
+    if (values.find(key) == values.end())
+        setStr(key, std::move(std::to_string(value)));
+}
+
 
 void configFile::save() {
     std::ofstream file(path);
--- Libraries/configManager/configManager.hpp
+++ Libraries/configManager/configManager.hpp
@@ -17,14 +17,21 @@ class configFile {
     
     std::map<std::string, std::string> values;
 public:
+    void loadConfig();
+
+
     explicit configFile(const std::string& path);
     configFile() = default;
     void save();
     ~configFile();
     
-    std::string get(const std::string& key);
-    void set(const std::string& key, std::string value);
-    void setDefault(const std::string& key, std::string value);
+    std::string getStr(const std::string& key);
+    int getInt(const std::string& key);
+    void setStr(const std::string& key, std::string value);
+    void setInt(const std::string& key, int value);
+
+    void setDefaultStr(const std::string& key, std::string value);
+    void setDefaultInt(const std::string& key, int value);
 };
 
 #endif /* configManager_hpp */
