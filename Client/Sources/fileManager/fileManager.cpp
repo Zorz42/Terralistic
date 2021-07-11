e6fdefd7eca594d33f2c14a1a032dbf803e38a62
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2021-07-11 13:44:36 +0200

    refactored file manager

--- Client/Sources/fileManager/fileManager.cpp
+++ Client/Sources/fileManager/fileManager.cpp
@@ -1,11 +1,5 @@
-//
-//  fileManager.cpp
-//  Terralistic
-//
-//  Created by Jakob Zorz on 27/04/2021.
-//
-
 #include <filesystem>
+#include <set>
 
 #include "fileManager.hpp"
 #include "platform_folders.h"
@@ -13,7 +7,6 @@
 static std::string data_path;
 
 void fileManager::init() {
-    // data path is path in filesystem, where terralistic worlds are saved and other things
     data_path = sago::getDataHome() + "/Terralistic/";
 
     std::filesystem::create_directory(data_path);
@@ -31,14 +24,26 @@ std::string fileManager::getWorldsPath() {
     return data_path + "worlds/";
 }
 
+std::string removeEndUntilChars(std::string string, std::set<char> chars) {
+    while(chars.count(string.back()) == 0 && !string.empty())
+        string.pop_back();
+    string.pop_back();
+    return string;
+}
+
+std::string returnRndUntilChars(std::string string, std::set<char> chars) {
+    int iter = (int)string.size() - 1;
+    while(chars.count(string[iter]) == 0 && iter != 0)
+        iter--;
+    return string.substr(iter + 1, string.size());
+}
+
+
 std::string fileManager::getResourcePath(std::string executable_path) {
-    while(executable_path[executable_path.size()-1] != '/' && executable_path[executable_path.size()-1] != '\\')
-        executable_path.pop_back();
-    executable_path.pop_back();
-    std::string parent_directory;
-    while(!executable_path.empty() && executable_path[executable_path.size()-1] != '/' && executable_path[executable_path.size()-1] != '\\') {
-        parent_directory.insert(parent_directory.begin(), executable_path[executable_path.size()-1]);
-        executable_path.pop_back();
-    }
-    return parent_directory == "MacOS" ? executable_path + "Resources/" : executable_path + parent_directory + "/Resources/";
+    std::string parent_directory = removeEndUntilChars(executable_path, {'/', '\\'});
+    
+    std::string parent_parent_directory = removeEndUntilChars(parent_directory, {'/', '\\'}) + "/";
+    std::string parent_directory_name = returnRndUntilChars(parent_directory, {'/', '\\'});
+    
+    return parent_directory_name == "MacOS" ? parent_parent_directory + "Resources/" : parent_parent_directory + parent_directory_name + "/Resources/";
 }
