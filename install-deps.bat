f9076278fd5cbf83a0721e6597fe8a95621cc984
NrdyBhu1 <NrdyBhu1@gmail.com>
2021-07-23 11:44:50 +0530

    Add install-deps files

---
+++ install-deps.bat
@@ -0,0 +1,12 @@
+set DEP_PATH=./Dependencies
+set SFML_PATH=%DEP_PATH%/SFML
+
+IF NOT EXIST %DEP_PATH% (
+  mkdir %DEP_PATH%
+)
+
+IF NOT EXIST %SFML_PATH% (
+  wget https://www.sfml-dev.org/files/SFML-2.5.1-windows-gcc-7.3.0-mingw-64-bit.zip
+  compat /u "SFML-2.5.1-windows-gcc-7.3.0-mingw-64-bit.zip" /i /Q
+  move SFML-2.5.1 %SFML_PATH%
+)
---
+++ install-deps.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+DEP_PATH=./Dependencies
+SFML_PATH=$DEP_PATH/SFML
+
+if test -d "$DEP_PATH"; then
+    echo
+else
+    mkdir "$DEP_PATH"
+fi
+
+curl_deps() {
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    curl https://www.sfml-dev.org/files/SFML-2.5.1-linux-gcc-64-bit.tar.gz --output sfml.tar.gz
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    curl -https://www.sfml-dev.org/files/SFML-2.5.1-macOS-clang.tar.gz --output sfml.tar.gz
+  else
+    echo Os Not Supported!
+  fi
+}
+
+if test -d "$SFML_PATH"; then
+    echo Deps already installed!
+else
+  curl_deps
+  tar -xvf sfml.tar.gz
+  mv SFML* $SFML_PATH
+  rm sfml.tar.gz
+fi
