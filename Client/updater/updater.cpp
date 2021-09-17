63ce1c6989e764144069022b88a56629b7c3c6cc
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2021-09-17 22:02:07 +0200

    added empty updater

---
+++ Client/updater/updater.cpp
@@ -0,0 +1,32 @@
+#include "updater.hpp"
+#include <SFML/Network.hpp>
+
+void checkForUpdatesMacOS();
+void checkForUpdatesWindows();
+void checkForUpdatesLinux();
+
+void checkForUpdates() {
+#ifdef _WIN32
+    checkForUpdatesWindows();
+#endif
+    
+#ifdef __APPLE__
+    checkForUpdatesMacOS();
+#endif
+    
+#ifdef __linux__
+    checkForUpdatesLinux();
+#endif
+}
+
+void checkForUpdatesMacOS() {
+    
+}
+
+void checkForUpdatesWindows() {
+    
+}
+
+void checkForUpdatesLinux() {
+    
+}
