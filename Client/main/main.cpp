104817f60932a85d9a803bccd719d03b909b4213
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2021-09-18 20:05:12 +0200

    added version print

--- Client/main/main.cpp
+++ Client/main/main.cpp
@@ -1,4 +1,5 @@
 #include <filesystem>
+#include <iostream>
 #include "mainMenu.hpp"
 #include "platform_folders.h"
 #include "configManager.hpp"
@@ -7,8 +8,14 @@
 #include "serverPlayers.hpp"
 #include "settings.hpp"
 #include "updater.hpp"
+#include "versions.hpp"
 
 int main(int argc, char **argv) {
+    if(argc == 2 && (std::string)argv[0] == "version") {
+        std::cout << CURR_VERSION_STR << std::endl;
+        return 0;
+    }
+    
     gfx::init(1130, 700);
     gfx::resource_path = getResourcePath(argv[0]);
     gfx::setMinimumWindowSize(gfx::getWindowWidth(), gfx::getWindowHeight());
