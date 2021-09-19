f438aaba28fc438913e2aa04ce81a858207a28eb
Zorz42 <jakob@zorz.si>
2021-09-19 17:24:55 +0200

    added possible updating to terralistic

--- Client/updater/updater.cpp
+++ Client/updater/updater.cpp
@@ -24,29 +24,31 @@ bool checkForUpdates(const std::string& exec_path) {
 #endif
 }
 
-bool checkForUpdatesMacOS(std::string exec_path) {
+void getPatchFromServer(const std::string& version) {
     sf::TcpSocket server_socket;
     if(server_socket.connect("jakob.zorz.si", 65431) != sf::Socket::Done)
-        return false;
-    
-    std::string version = "MacOS-";
-    version += CURR_VERSION_STR;
+        return;
+
     server_socket.send(version.c_str(), version.size());
-    
+
     std::string buffer;
     char temp_buffer[1024];
     std::size_t received;
     while(server_socket.receive(temp_buffer, 1024, received) == sf::Socket::Done) {
         buffer.insert(buffer.end(), &temp_buffer[0], &temp_buffer[0] + received);
     }
-    
-    while(exec_path.back() != '/')
-        exec_path.pop_back();
-    
+
     if(!buffer.empty()) {
         std::ofstream patch_file(sago::getDataHome() + "/Terralistic/update.patch", std::ios::binary | std::ios::trunc);
         patch_file.write(&buffer[0], buffer.size());
     }
+}
+
+bool checkForUpdatesMacOS(std::string exec_path) {
+    getPatchFromServer((std::string)"MacOS-" + CURR_VERSION_STR);
+
+    while(exec_path.back() != '/')
+        exec_path.pop_back();
     
     if(std::filesystem::exists(sago::getDataHome() + "/Terralistic/update.patch")) {
         std::system(((std::string)"cd \"" + exec_path + "../..\" && patch -t -p3 < \"" + sago::getDataHome() + "/Terralistic/update.patch\"").c_str());
@@ -62,5 +64,16 @@ bool checkForUpdatesWindows(std::string exec_path) {
 }
 
 bool checkForUpdatesLinux(std::string exec_path) {
+    getPatchFromServer((std::string)"Linux-" + CURR_VERSION_STR);
+
+    while(exec_path.back() != '/')
+        exec_path.pop_back();
+
+    if(std::filesystem::exists(sago::getDataHome() + "/Terralistic/update.patch")) {
+        std::system(((std::string)"cd \"" + exec_path + "\" && patch -t -p3 < \"" + sago::getDataHome() + "/Terralistic/update.patch\"").c_str());
+        std::filesystem::remove(sago::getDataHome() + "/Terralistic/update.patch");
+        return true;
+    }
+
     return false;
 }
