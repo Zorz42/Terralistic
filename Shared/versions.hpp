aa3b9e6c07316a60e0b74bcef0097d6f7af314ff
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2021-09-19 11:44:09 +0200

    improved updating probably

--- Shared/versions.hpp
+++ Shared/versions.hpp
@@ -1,4 +1,4 @@
-enum class Version { ALPHA_0_0_0, ALPHA_0_1_0, ALPHA_0_1_1, ALPHA_0_2_0, ALPHA_0_2_1, ALPHA_0_2_2, ALPHA_0_2_3, ALPHA_0_2_4, ALPHA_0_2_5, ALPHA_0_2_6, ALPHA_0_2_7 };
+enum class Version { ALPHA_0_0, ALPHA_0_1, ALPHA_0_2 };
 
-#define CURR_VERSION Version::ALPHA_0_2_7
-#define CURR_VERSION_STR "0.2.7-alpha"
+#define CURR_VERSION Version::ALPHA_0_2
+#define CURR_VERSION_STR "0.2.9-alpha"
