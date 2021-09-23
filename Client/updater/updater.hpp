47ec4cb4db7e4a3f768ddc12dbe7552a4d2984d6
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2021-09-23 07:54:01 +0200

    improved updater with gui

--- Client/updater/updater.hpp
+++ Client/updater/updater.hpp
@@ -1,6 +1,25 @@
 #ifndef updater_hpp
 #define updater_hpp
 
-bool checkForUpdates(const std::string& exec_path);
+#include <thread>
+#include "graphics.hpp"
+#include "menuBack.hpp"
+
+enum class UpdateState {NEUTRAL, CHECKING, DOWNLOADING, APPLYING, FINISHED};
+
+class UpdateChecker : public gfx::Scene {
+    void checkForUpdates();
+    UpdateState update_state = UpdateState::NEUTRAL;
+    std::thread update_thread;
+    std::string exec_path;
+    gfx::Sprite text;
+    bool has_updated = false;
+    void init() override;
+    void render() override;
+    MenuBack* menu_back;
+public:
+    UpdateChecker(MenuBack* menu_back, std::string exec_path) : menu_back(menu_back), exec_path(exec_path) {}
+    bool hasUpdated() { return has_updated; }
+};
 
 #endif
