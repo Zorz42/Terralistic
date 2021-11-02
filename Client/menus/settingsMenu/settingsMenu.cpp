cd015ae2d35272327fca5b4b6f9cdbbf1d357e56
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2021-11-02 08:52:13 +0100

    added scale setting back

--- Client/menus/settingsMenu/settingsMenu.cpp
+++ Client/menus/settingsMenu/settingsMenu.cpp
@@ -33,6 +33,8 @@ bool SettingsMenu::onKeyDown(gfx::Key key) {
     if(key == gfx::Key::MOUSE_LEFT) {
         if(back_button.isHovered(getMouseX(), getMouseY()))
             returnFromScene();
+        for(RenderSetting* setting : render_settings)
+            setting->onMouseButtonDown(getMouseX(), getMouseY());
         return true;
     }
     return false;
@@ -51,7 +53,7 @@ void SettingsMenu::render() {
         c.a = TRANSPARENCY;
         gfx::RectShape(gfx::getWindowWidth() / 2 - width / 2, y, width, height).render(c);
         
-        setting->render(y);
+        setting->render(y, required_width, getMouseX(), getMouseY());
         
         y += height;
     }
@@ -61,17 +63,61 @@ void SettingsMenu::render() {
 }
 
 RenderChoiceSetting::RenderChoiceSetting(ChoiceSetting* setting) : setting(setting) {
-    
+    choice_text.loadFromText(setting->indent);
+    choice_text.scale = 3;
+    choice_text.orientation = gfx::TOP;
+    select_rect.border_color = GREY;
+    select_rect.smooth_factor = 2;
+    select_rect.orientation = gfx::TOP;
+    for(const std::string& choice : setting->choices) {
+        gfx::Button* button = new gfx::Button;
+        button->loadFromText(choice);
+        button->scale = 2;
+        button->orientation = gfx::TOP;
+        button->margin = 5;
+        choice_buttons.push_back(button);
+    }
 }
 
-void RenderChoiceSetting::render(int y) {
+void RenderChoiceSetting::render(int y, int width, int mouse_x, int mouse_y) {
+    gfx::Button* selected_button = choice_buttons[setting->getSelectedChoice()];
+    select_rect.setX(selected_button->x);
+    select_rect.setY(selected_button->y);
+    select_rect.setWidth(selected_button->getWidth());
+    select_rect.setHeight(selected_button->getHeight());
+    select_rect.render();
     
+    choice_text.y = y + getHeight() / 2 - choice_text.getHeight() / 2;
+    choice_text.x = -width / 2 + choice_text.getWidth() / 2 + SPACING;
+    
+    choice_text.render();
+    
+    int x = width / 2 - SPACING;
+    for(int i = (int)choice_buttons.size() - 1; i >= 0; i--) {
+        gfx::Button* button = choice_buttons[i];
+        x -= button->getWidth() / 2;
+        button->x = x;
+        x -= button->getWidth() / 2;
+        button->y = y + getHeight() / 2 - button->getHeight() / 2;
+        button->render(mouse_x, mouse_y);
+    }
 }
 
 int RenderChoiceSetting::getHeight() {
-    return 100;
+    int height = 0;
+    height = std::max(height, choice_text.getHeight() + 2 * SPACING);
+    return height;
 }
 
 int RenderChoiceSetting::getWidth() {
-    return 500;
+    int width = choice_text.getWidth() + 3 * SPACING;
+    for(const gfx::Button* button : choice_buttons)
+        width += button->getWidth();
+    return width;
+}
+
+void RenderChoiceSetting::onMouseButtonDown(int x, int y) {
+    for(int i = 0; i < choice_buttons.size(); i++)
+        if(choice_buttons[i]->isHovered(x, y))
+            setting->setSelectedChoice(i);
 }
--- Client/menus/settingsMenu/settingsMenu.hpp
+++ Client/menus/settingsMenu/settingsMenu.hpp
@@ -6,18 +6,23 @@
 
 class RenderSetting {
 public:
-    virtual void render(int y) = 0;
+    virtual void render(int y, int width, int mouse_x, int mouse_y) = 0;
     virtual int getHeight() = 0;
     virtual int getWidth() = 0;
+    virtual void onMouseButtonDown(int x, int y) = 0;
     virtual ~RenderSetting() {}
 };
 
 class RenderChoiceSetting : public RenderSetting {
-    const ChoiceSetting* setting;
+    ChoiceSetting* const setting;
+    std::vector<gfx::Button*> choice_buttons;
+    gfx::Sprite choice_text;
+    gfx::Rect select_rect;
+    void onMouseButtonDown(int x, int y) override;
 public:
     RenderChoiceSetting(ChoiceSetting* setting);
     
-    void render(int y) override;
+    void render(int y, int width, int mouse_x, int mouse_y) override;
     int getHeight() override;
     int getWidth() override;
 };
