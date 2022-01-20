0b8dfc7f13a6aa4a66d15ede2e8bfec6e6e41e37
iKramp <nejc.susnik05@gmail.com>
2022-01-20 21:35:44 +0100

    TP now works in all forms (Player to player, executor to coordinate, player to coordinate, executor to player) and reports command errors back

--- Server/commands/commands.cpp
+++ Server/commands/commands.cpp
@@ -32,33 +32,110 @@ int formatCoord(std::string coord_str, int curr_coord) {
 }
 
 void TpCommand::onCommand(const std::vector<std::string>& args, ServerPlayer* executor) {
-    if(args.size() >= 2) {
-        int x_coord = formatCoord(args[0], executor->getX() / 16), y_coord = formatCoord(args[1], -executor->getY() / 16 + blocks->getHeight());
-        ServerPlayer* to_teleport = executor;
-        if(args.size() >= 3)
-            to_teleport = players->getPlayerByName(args[2]);
+    if(args.size() == 1 && !(std::all_of(args[0].begin(), args[0].end(), ::isdigit) || args[0].at(0) == '~')){
+        ServerPlayer* destination = players->getPlayerByName(args[0]);
+        if(destination == nullptr){
+            sf::Packet error_message;
+            error_message << ServerPacketType::CHAT << "Player with name " + args[0] + " does not exist";
+            executor->getConnection()->send(error_message);
+            return;
+        }
+        entities->setX(executor, destination->getX());
+        entities->setY(executor, destination->getY());
         sf::Packet feedback_message;
-        feedback_message << ServerPacketType::CHAT << "Successfully teleported " + to_teleport->name + " to " + std::to_string(x_coord) + " " + std::to_string(y_coord);
-        //feedback_message << ServerPacketType::CHAT <<
+        feedback_message << ServerPacketType::CHAT << "Successfully teleported " + executor->name + " to " + destination->name;
         executor->getConnection()->send(feedback_message);
-        y_coord = -y_coord + blocks->getHeight();
-        entities->setX(to_teleport, x_coord * 16);
-        entities->setY(to_teleport, y_coord * 16);
         return;
     }
+    if(args.size() == 2 || args.size() == 3){
+        int x_coord, y_coord;
+        ServerPlayer *to_teleport = nullptr;
+        if(std::all_of(args[0].begin(), args[0].end(), ::isdigit) || args[0].at(0) == '~'){
+            if(!std::all_of(args[1].begin(), args[1].end(), ::isdigit)) {
+                sf::Packet error_message;
+                error_message << ServerPacketType::CHAT << "Arguments incorrect. Use /help tp for a list of arguments";
+                executor->getConnection()->send(error_message);
+                return;
+            }
+            x_coord = formatCoord(args[0], executor->getX() / 16);
+            y_coord = formatCoord(args[1], -executor->getY() / 16 + blocks->getHeight());
+
+            sf::Packet feedback_message;
+            feedback_message << ServerPacketType::CHAT << "Successfully teleported " + executor->name + " to " + std::to_string(x_coord) + " " + std::to_string(y_coord);
+            executor->getConnection()->send(feedback_message);
+
+            y_coord = -y_coord + blocks->getHeight();
+            entities->setX(executor, x_coord * 16);
+            entities->setY(executor, y_coord * 16);
+            return;
+        }else
+            to_teleport = players->getPlayerByName(args[0]);
+        if(to_teleport == nullptr) {
+            sf::Packet error_message;
+            error_message << ServerPacketType::CHAT << "Player with name " + args[0] + " does not exist";
+            executor->getConnection()->send(error_message);
+            return;
+        }
+        else
+            std::destroy(args.begin(), args.begin());
+
+        if(args.size() == 1){
+            if(std::all_of(args[0].begin(), args[0].end(), ::isdigit)) {
+                sf::Packet error_message;
+                error_message << ServerPacketType::CHAT << "Arguments incorrect. Use /help tp for a list of arguments";
+                executor->getConnection()->send(error_message);
+                return;
+            }
+            ServerPlayer* destination = players->getPlayerByName(args[0]);
+            if(destination == nullptr) {
+                sf::Packet error_message;
+                error_message << ServerPacketType::CHAT << "Player with name " + args[0] + "does not exist";
+                executor->getConnection()->send(error_message);
+                return;
+            }
+            entities->setX(to_teleport, destination->getX());
+            entities->setY(to_teleport, destination->getY());
+            sf::Packet feedback_message;
+            feedback_message << ServerPacketType::CHAT << "Successfully teleported " + to_teleport->name + " to " + destination->name;
+            executor->getConnection()->send(feedback_message);
+            return;
+        }
+        x_coord = formatCoord(args[0], executor->getX() / 16);
+        y_coord = formatCoord(args[1], -executor->getY() / 16 + blocks->getHeight());
+
+        sf::Packet feedback_message;
+        feedback_message << ServerPacketType::CHAT << "Successfully teleported " + executor->name + " to " + std::to_string(x_coord) + " " + std::to_string(y_coord);
+        executor->getConnection()->send(feedback_message);
+
+        y_coord = -y_coord + blocks->getHeight();
+        entities->setX(executor, x_coord * 16);
+        entities->setY(executor, y_coord * 16);
+    }
     sf::Packet error_message;
-    error_message << ServerPacketType::CHAT << "Arguments not formatted correctly. Type /help tp for a list of formats.";
+    error_message << ServerPacketType::CHAT << "Arguments incorrect. Use /help tp for a list of arguments";
     executor->getConnection()->send(error_message);
 }
 
 void GiveCommand::onCommand(const std::vector<std::string>& args, ServerPlayer* executor) {
-    if(args.size() >= 1) {
-        ItemType* item = items->getItemTypeByName(args[0]);
+    if(args.size() == 1 || args.size() == 2) {
         int quantity = 1;
-        if(args.size() >= 2)
+        if(args.size() == 2) {
             quantity = std::stoi(args[1]);
+            std::destroy(args.begin(), args.begin());
+        }
+        ItemType* item = items->getItemTypeByName(args[0]);
+        if(item == nullptr){
+            sf::Packet error_message;
+            error_message << ServerPacketType::CHAT << "Item with name " + args[0] + "does not exist";
+            executor->getConnection()->send(error_message);
+            return;
+        }
         executor->inventory.addItem(item, quantity);
+        return;
     }
+    sf::Packet error_message;
+    error_message << ServerPacketType::CHAT << "Arguments incorrect. Use /help tp for a list of arguments";
+    executor->getConnection()->send(error_message);
 }
 
 void SetHealthCommand::onCommand(const std::vector<std::string>& args, ServerPlayer* executor) {
