c94988e719cbe73143da84790d3556df051eafb3
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2022-01-21 19:48:53 +0100

    Update versions.hpp

--- Shared/versions.hpp
+++ Shared/versions.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
 #define CURR_VERSION Version::ALPHA_0_3
-#define CURR_VERSION_STR "0.3.3-alpha"
+#define CURR_VERSION_STR "0.3.4-alpha"
 
 enum class Version { ALPHA_0_0, ALPHA_0_1, ALPHA_0_2, ALPHA_0_3 };
