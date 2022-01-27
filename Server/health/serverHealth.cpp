0020fa6318a2953f6c84d5d6659877ee781fbc25
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2022-01-27 22:07:08 +0100

    shortened some code in commands

--- Server/health/serverHealth.cpp
+++ Server/health/serverHealth.cpp
@@ -8,7 +8,7 @@ void ServerHealth::onEvent(EntityAbsoluteVelocityChangeEvent &event) {
     ServerPlayer* curr_player = players->getPlayerById(event.entity->id);
     int delta_vel_x = std::abs(curr_player->getVelocityX() - event.old_vel_x);
     int delta_vel_y = std::abs(curr_player->getVelocityY() - event.old_vel_y);
-    if(delta_vel_x + delta_vel_y > 99){
+    if(delta_vel_x + delta_vel_y > 99) {
         int i = 1;
     }
 }
