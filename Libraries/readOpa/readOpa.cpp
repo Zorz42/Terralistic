295fc4df492ec4c7659f6abdd7476070b461796e
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2022-06-07 10:11:21 +0200

    fixed windows texture loading

--- Libraries/readOpa/readOpa.cpp
+++ Libraries/readOpa/readOpa.cpp
@@ -3,8 +3,10 @@
 #include "readOpa.hpp"
 #include "exception.hpp"
 
+#include <iostream>
+
 void loadOpa(gfx::Texture& texture, const std::string& path) {
-    std::ifstream opa_file(path);
+    std::ifstream opa_file(path, std::ios::binary);
     if(!opa_file.is_open() || opa_file.eof() && opa_file.fail())
         throw Exception("Could not open file: " + path);
 
@@ -16,10 +18,10 @@ void loadOpa(gfx::Texture& texture, const std::string& path) {
     opa_file.read((char*)&data[0], file_size);
 
     int width = *(int*)&data[0];
-    int height = *(int*)&data[4];
-    data.erase(data.begin(), data.begin() + 8);
+    int height = *(int*)&data[sizeof(int)];
+    data.erase(data.begin(), data.begin() + sizeof(int) * 2);
     if(data.size() != width * height * 4)
         throw Exception("OPA data size error file size is " + std::to_string(data.size()) + " but should be " + std::to_string(width * height * 4));
-    
+
     texture.loadFromData(&data[0], width, height);
 }
