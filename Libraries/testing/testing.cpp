dfbfad2079447e4fb815cb5c3c657493f702404f
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2022-07-14 00:19:39 +0200

    added cases to tests

--- Libraries/testing/testing.cpp
+++ Libraries/testing/testing.cpp
@@ -12,23 +12,32 @@ _TestClass::_TestClass() {
 
 void _TestClass::_performTests() {
     std::cout << "Performing tests for [" << class_name << "]" << std::endl;
-    for(_TestCase* test_case : test_cases)
-        test_case->performTest();
+    for(_TestCase& test_case : test_cases)
+        test_case.performTest();
 }
 
 void _TestCase::performTest() {
     std::cout << "Case " << case_name << std::endl;
+    (*instance.*case_func)();
 }
 
 _TestClassNameSetter::_TestClassNameSetter(_TestClass* test, const std::string& name) {
     test->class_name = name;
 }
 
-TEST_CLASS {
+TEST_CLASS
     TEST_CASE(TestTest) {
+        std::cout << "testing" << std::endl;
+    }
+TEST_NAME(Test)
+
+struct A {
+    typedef A self;
+    void (self::*a)() = &self::test_func;
+    void test_func() {
         
     }
-} TEST_NAME(Test)
+};
 
 void performTests() {
     if(test_classes == nullptr) {
--- Libraries/testing/testing.hpp
+++ Libraries/testing/testing.hpp
@@ -2,16 +2,21 @@
 #include <string>
 #include <vector>
 
+class _TestClass;
+
 class _TestCase {
     std::string case_name;
+    void (_TestClass::* case_func)();
+    _TestClass* instance;
 public:
-    _TestCase(const std::string& case_name) : case_name(case_name) {}
+    _TestCase(void (_TestClass::* case_func)(), const std::string& case_name, _TestClass* instance) : case_func(case_func), case_name(case_name), instance(instance) {}
     void performTest();
 };
 
 class _TestClass {
     friend class _TestClassNameSetter;
-    std::vector<_TestCase*> test_cases;
+    friend class _CaseRegistrator;
+    std::vector<_TestCase> test_cases;
     std::string class_name;
 public:
     _TestClass();
@@ -27,15 +32,26 @@ public:
     _TestClassNameSetter(_TestClass* test, const std::string& name);
 };
 
+class _CaseRegistrator {
+public:
+    _CaseRegistrator(void (_TestClass::* case_func)(), _TestClass* test_class, const std::string& case_name) {
+        test_class->test_cases.push_back(_TestCase(case_func, case_name, test_class));
+    }
+};
+
 void _setTestName(_TestClass* test, const std::string& name);
 
-#define _TEST_CLASS(line) static class _TestClassInstance ## line : public _TestClass
+#define _TEST_CLASS(line) static class _TestClassInstance ## line : public _TestClass { \
+typedef _TestClassInstance ## line self;
+
 #define _TEST_CLASSP(line) _TEST_CLASS(line)
 #define TEST_CLASS _TEST_CLASSP(__LINE__)
 
-#define TEST_CASE(case_name) void case_name()
+#define _TEST_CASE(name, line) _CaseRegistrator case_registrator ## line = _CaseRegistrator((void (_TestClass::*)())&self::case_name, this, #name); void case_name()
+#define _TEST_CASEP(name, line) _TEST_CASE(name, line)
+#define TEST_CASE(name) _TEST_CASEP(name, __LINE__)
 
-#define _TEST_NAME(name, line) name; static _TestClassNameSetter test_name_setter ## line (&name, #name);
+#define _TEST_NAME(name, line) } name; static _TestClassNameSetter test_name_setter ## line (&name, #name);
 #define _TEST_NAMEP(name, line) _TEST_NAME(name, line)
 #define TEST_NAME(name) _TEST_NAMEP(name, __LINE__)
 
