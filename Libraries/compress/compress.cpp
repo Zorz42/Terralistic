45a9ee1a09709ba6014889f9cad25872348c2423
Jakob Zorz <54270248+Zorz42@users.noreply.github.com>
2022-08-30 14:52:57 +0200

    improved compression library a little bit

--- Libraries/compress/compress.cpp
+++ Libraries/compress/compress.cpp
@@ -6,15 +6,17 @@
 #include <iostream>
 #include "graphics.hpp"
 
+#define COMPRESSION_QUALITY 1.f
+
 std::vector<char> compress(const std::vector<char>& decompressed_data) {
     gfx::Timer timer;
     
     int max_compressed_size = LZ4_compressBound((int)decompressed_data.size());
     std::vector<char> compressed_data(max_compressed_size);
     
-    //if(compress((Bytef*)&compressed_data[0], &compressed_size, (const Bytef*)&decompressed_data[0], decompressed_data.size()) != Z_OK) throw CompressError("Could not archive some data!");
+    int compressed_size = LZ4_compress_fast(&decompressed_data[0], &compressed_data[0], (int)decompressed_data.size(), max_compressed_size, (1 - COMPRESSION_QUALITY) * 65537);
     
-    int compressed_size = LZ4_compress_fast(&decompressed_data[0], &compressed_data[0], (int)decompressed_data.size(), max_compressed_size, 0);
+    if(compressed_size <= 0) throw CompressError("Could not archive some data!");
     
     compressed_data.resize(compressed_size + sizeof(unsigned int));
     
@@ -22,7 +24,7 @@ std::vector<char> compress(const std::vector<char>& decompressed_data) {
     for(int i = 0; i < sizeof(unsigned int); i++)
         compressed_data[compressed_size + i] = decompressed_size >> 8 * i;
     
-    std::cout << "compress " << timer.getTimeElapsed() << "ms" << std::endl;
+    //std::cout << "compress " << timer.getTimeElapsed() << "ms" << std::endl;
     
     return compressed_data;
 }
@@ -40,7 +42,7 @@ std::vector<char> decompress(const std::vector<char>& compressed_data) {
     
     decompressed_size = LZ4_decompress_safe(&compressed_data[0], &decompressed_data[0], (int)compressed_data.size() - sizeof(unsigned int), actual_decompressed_size);
     
-    //if(uncompress((Bytef*)&decompressed_data[0], &actual_decompressed_size, (const Bytef*)&compressed_data[0], compressed_data.size() - sizeof(unsigned int)) != Z_OK) throw ArchiveError("Archive is corrupted!");
+    if(decompressed_size <= 0) throw ArchiveError("Archive is corrupted!");
     
     if(actual_decompressed_size != decompressed_size) throw ArchiveError("Uncompressed sizes did not match!");
     
