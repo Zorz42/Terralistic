633778f1b41e67660486af4d2a438486c87e9066
Jakob Zorz <54270248+Zorz42@users.noreply.github.com>
2022-10-11 07:35:33 +0200

    made generation bar more accurate

--- Server/worldGenerator/worldGenerator.cpp
+++ Server/worldGenerator/worldGenerator.cpp
@@ -19,7 +19,7 @@ int WorldGenerator::generateWorld(int world_width, int world_height, int seed) {
     if(seed == 1000) {
         generateStructureWorld();
     } else {
-        generating_total = blocks->getWidth() * 4;
+        generating_total = blocks->getWidth() * 6 + blocks->getWidth() / 8;
         loadBiomes();
         generateDefaultWorld(noise, seeded_random);
     }
@@ -95,6 +95,7 @@ void WorldGenerator::placeStructures(siv::PerlinNoise &noise) {
                 checking_structure.x_of_last_instance = x;
             }
         }
+        generating_current++;
     }
 }
 
@@ -389,6 +390,7 @@ void WorldGenerator::generateFoliage(std::mt19937& seeded_random) {
             else if(seeded_random() % 3 == 0)
                 blocks->setBlockTypeSilently(x, blocks->getHeight() - surface_heights[x] - 1, &content->blocks.grass);
         }
+        generating_current++;
     }
 }
 
@@ -486,8 +488,8 @@ void WorldGenerator::placeWalls() {
             if (y > blocks->getHeight() - surface_heights[std::max(x - 1, 0)] && y > blocks->getHeight() - surface_heights[x] &&
                 y > blocks->getHeight() - surface_heights[std::min(x + 1, blocks->getWidth() - 1)])
                 walls->setWallTypeSilently(x, y, &content->walls.dirt);
-        /*if(x % 8 == 0)
-            generating_current++;*/
+        if(x % 8 == 0)
+            generating_current++;
     }
 }
 
