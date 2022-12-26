7708108f0349efcb3ca26816dbdfb33089096c87
Jakob Zorz <54270248+Zorz42@users.noreply.github.com>
2022-12-26 22:48:50 +0100

    added lua minifier to compiling mods

--- BuildProject/compile_mod.rs
+++ BuildProject/compile_mod.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 use std::path::PathBuf;
+use darklua_core::generator::{DenseLuaGenerator, LuaGenerator};
+use darklua_core::Parser;
 use shared::mod_manager::{GameMod, GameModData};
 
 /**
@@ -17,7 +19,17 @@ pub fn compile_mod(mod_path: PathBuf) {
     // read the mod's lua code
     let lua_code = std::fs::read_to_string(mod_path.join("mod.lua")).unwrap();
 
-    let mod_obj = GameMod::new(GameModData::new(lua_code, HashMap::new()));
+
+    //Use darklua to minify the mod's lua code. Use DenseLuaGenerator to generate the minified code.
+    let mut parser = Parser::default();
+    let block = parser.parse(&lua_code).unwrap();
+
+    let mut generator = DenseLuaGenerator::default();
+    generator.write_block(&block);
+
+    let minified_lua_code = generator.into_string();
+
+    let mod_obj = GameMod::new(GameModData::new(minified_lua_code, HashMap::new()));
 
     // write the mod to a file that has the same name as the mod's directory and a .mod extension
     std::fs::write(mod_path.join(format!("{}.mod", mod_path.file_name().unwrap().to_str().unwrap())), mod_obj.to_bytes()).unwrap();
