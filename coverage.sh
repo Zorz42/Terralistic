c74a463ecea6c32d551cf425b13e4529f9c05140
Jakob Zorz <54270248+Zorz42@users.noreply.github.com>
2023-04-02 17:30:44 +0200

    Update coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -1,4 +1,5 @@
-CARGO_INCREMENTAL=0 LLVM_PROFILE_FILE='cargo-test-%p-%m.profraw' cargo test &&
+rm *.profraw
+CARGO_INCREMENTAL=0 LLVM_PROFILE_FILE='cargo-test-%p-%m.profraw' cargo test
 rm -r target/coverage/html/
-grcov . --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/html &&
+grcov . --binary-path ./target/debug/deps/ -s . -t html --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/html
 rm *.profraw
