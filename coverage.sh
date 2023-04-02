1e92bdbf71a0d7367bedcd3c228018e4b9e8f6f7
Jakob Zorz <54270248+Zorz42@users.noreply.github.com>
2023-04-02 18:36:14 +0200

    Update coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -1,5 +1,5 @@
 rm *.profraw
-CARGO_INCREMENTAL=0 LLVM_PROFILE_FILE='cargo-test-%p-%m.profraw' cargo test
+CARGO_INCREMENTAL=0 LLVM_PROFILE_FILE='cargo-test-%p-%m.profraw' cargo --config="../.cargo/config-coverage.toml" test
 rm -r target/coverage/html/
 grcov . --binary-path ./target/debug/deps/ -s . -t html --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/html
 rm *.profraw
