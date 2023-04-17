355274e70466df4760184ebeefac6adbaa0df62d
iKramp <nejc.susnik05@gmail.com>
2023-04-17 18:51:46 +0200

    changed for ease of use

--- server/server_core/commands.rs
+++ server/server_core/commands.rs
@@ -1,11 +1,20 @@
 use crate::server::server_ui::ServerState;
 
+/**
+ * This struct contains all parameters that are needed to execute any command
+ * It is used so that when a new argument is needed to be added to a command, it can be added here and all commands will be updated
+ */
+pub struct CommandParameters<'a> {
+    pub command_manager: &'a CommandManager,
+    pub state: &'a mut ServerState,
+}
+
 //struct that contains the command name and the function that will be executed when the command is called
 pub struct Command {
     pub call_name: String,
     pub name: String,
     pub description: String,
-    pub function: fn(&CommandManager, &mut ServerState) -> String,
+    pub function: fn(CommandParameters) -> String,
 }
 
 //contains all the commands
@@ -29,7 +38,10 @@ impl CommandManager {
     pub fn execute_command(&self, command: &str, state: &mut ServerState) -> String {
         for c in &self.commands {
             if c.call_name == command {
-                let feedback = (c.function)(&self, state);
+                let feedback = (c.function)(CommandParameters {
+                    command_manager: self,
+                    state
+                });
                 return feedback;
             }
         }
@@ -38,16 +50,16 @@ impl CommandManager {
 }
 
 //help command
-pub fn help(command_manager: &CommandManager, _state: &mut ServerState) -> String {
+pub fn help(parameters: CommandParameters) -> String {
     let mut string = String::new();
     string.push_str("Commands:\n");
-    for c in &command_manager.commands {
+    for c in &parameters.command_manager.commands {
         string.push_str(&format!("{} - {}\n", c.call_name, c.description));
     }
     string
 }
 
-pub fn stop(_command_manager: &CommandManager, state: &mut ServerState) -> String {
-    *state = ServerState::Stopping;
+pub fn stop(parameters: CommandParameters) -> String {
+    *parameters.state = ServerState::Stopping;
     String::from("Stopping server...")
 }
