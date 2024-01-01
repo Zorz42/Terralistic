a022d35618fa89bad207782f5f9b50761ef1de69
iKramp <nejc.susnik05@gmail.com>
2024-01-01 17:33:01 +0100

    i'm probably making bad decisions but... oh well

--- client/game/tls_client.rs
+++ client/game/tls_client.rs
@@ -2,15 +2,26 @@ use crate::shared::tls_client;
 use crate::shared::tls_client::ConnectionState;
 use anyhow::Result;
 
+#[allow(non_camel_case_types)] //shut up clippy
+pub enum AuthenticationState {
+    NOT_AUTHENTICATED,
+    AUTHENTICATING,
+    AUTHENTICATED,
+    NO_CREDENTIALS,
+    FAILED,
+}
+
 pub struct TlsClient {
-    is_authenticated: bool,
+    user_credentials: Option<(String, String)>,
+    authentication_state: AuthenticationState,
     client: tls_client::TlsClient,
 }
 
 impl TlsClient {
     pub fn new() -> Result<Self> {
         Ok(Self {
-            is_authenticated: false,
+            user_credentials: None,
+            authentication_state: AuthenticationState::NO_CREDENTIALS, //read from file and set this to NOT_AUTHENTICATED if the password is saved
             client: tls_client::TlsClient::new()?,
         })
     }
@@ -29,23 +40,36 @@ impl TlsClient {
 
     pub fn close(&mut self) {
         self.client.close();
+        self.authentication_state = AuthenticationState::NOT_AUTHENTICATED;
     }
 
     pub fn authenticate(&mut self) {
+        //this is ugly lol
         match self.client.get_state() {
             ConnectionState::FAILED(_) => {}
             ConnectionState::DISCONNECTED | ConnectionState::CONNECTING(_) => self.client.connect(),
-            ConnectionState::CONNECTED(_) => {
-                let res = self.client.write("authenticate me");
-                if res.is_err() {
-                    self.client.close();
+            ConnectionState::CONNECTED(_) => match &self.authentication_state {
+                AuthenticationState::NOT_AUTHENTICATED => match &self.user_credentials {
+                    Some((username, password)) => {
+                        self.client.write(&format!("{} {}", username, password)); //TODO handle errors
+                        self.authentication_state = AuthenticationState::AUTHENTICATING;
+                    }
+                    None => {
+                        self.authentication_state = AuthenticationState::NO_CREDENTIALS;
+                    }
+                },
+                AuthenticationState::AUTHENTICATING => {
+                    if let Ok(message) = self.client.read() {
+                        if message == "auth_success" {
+                            self.authentication_state = AuthenticationState::AUTHENTICATED;
+                        } else if message == "auth_failed" {
+                            self.authentication_state = AuthenticationState::FAILED;
+                        }
+                    }
                 }
-                self.is_authenticated = true;
-                self.close();
-            }
+                AuthenticationState::AUTHENTICATED | AuthenticationState::NO_CREDENTIALS | AuthenticationState::FAILED => {}
+            },
         }
-
-        self.is_authenticated = true;
     }
 
     #[must_use]
@@ -54,7 +78,7 @@ impl TlsClient {
     }
 
     #[must_use]
-    pub const fn is_authenticated(&self) -> bool {
-        self.is_authenticated
+    pub const fn is_authenticated(&self) -> &AuthenticationState {
+        &self.authentication_state
     }
 }
