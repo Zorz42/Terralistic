308421e5a3e39d666864bb37a6faee84c5cc6a8c
iKramp <nejc.susnik05@gmail.com>
2024-02-18 23:36:33 +0100

    made it so structs cannot implement update and render

--- libraries/graphics/ui_element.rs
+++ libraries/graphics/ui_element.rs
@@ -1,9 +1,13 @@
 use crate::libraries::graphics as gfx;
 
-pub trait UiElement {
-    fn get_sub_elements_mut(&mut self) -> Vec<&mut dyn UiElement>;
-    fn get_sub_elements(&self) -> Vec<&dyn UiElement>;
-    fn render_inner(&mut self, graphics: &gfx::GraphicsContext, parent_container: Option<&gfx::Container>);
+pub trait BaseUiElement: UiElement {
+    fn update(&mut self, graphics: &mut gfx::GraphicsContext, parent_container: Option<&gfx::Container>) {
+        self.update_inner(graphics, parent_container);
+        let container = self.get_container(graphics, parent_container);
+        for element in self.get_sub_elements_mut() {
+            element.update(graphics, Some(&container));
+        }
+    }
     fn render(&mut self, graphics: &gfx::GraphicsContext, parent_container: Option<&gfx::Container>) {
         self.render_inner(graphics, parent_container);
         let container = self.get_container(graphics, parent_container);
@@ -12,14 +16,15 @@ pub trait UiElement {
             element.render(graphics, Some(&container));
         }
     }
+}
+
+impl<T: UiElement> BaseUiElement for T {}
+
+pub trait UiElement {
+    fn get_sub_elements_mut(&mut self) -> Vec<&mut dyn BaseUiElement>;
+    fn get_sub_elements(&self) -> Vec<&dyn BaseUiElement>;
+    fn render_inner(&mut self, graphics: &gfx::GraphicsContext, parent_container: Option<&gfx::Container>);
     fn update_inner(&mut self, graphics: &mut gfx::GraphicsContext, parent_container: Option<&gfx::Container>);
-    fn update(&mut self, graphics: &mut gfx::GraphicsContext, parent_container: Option<&gfx::Container>) {
-        self.update_inner(graphics, parent_container);
-        let container = self.get_container(graphics, parent_container);
-        for element in self.get_sub_elements_mut() {
-            element.update(graphics, Some(&container));
-        }
-    }
     fn on_event(&mut self, graphics: &mut gfx::GraphicsContext, event: &gfx::Event, parent_container: Option<&gfx::Container>);
     fn get_container(&self, graphics: &gfx::GraphicsContext, parent_container: Option<&gfx::Container>) -> gfx::Container;
 }
